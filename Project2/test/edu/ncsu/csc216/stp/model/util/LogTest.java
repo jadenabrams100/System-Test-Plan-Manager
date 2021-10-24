27459dd422b1fe2292ab7e164e8d34a6416dd1a0
Jaden Abrams <jlabrams@ncsu.edu>
2021-10-24 17:45:32 -0400

    [Test] Created unfinished skeleton for Log

---
+++ Project2/test/edu/ncsu/csc216/stp/model/util/LogTest.java
@@ -0,0 +1,29 @@
+package edu.ncsu.csc216.stp.model.util;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class LogTest {
+
+	@Test
+	void testLog() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGet() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSize() {
+		fail("Not yet implemented");
+	}
+
+}
