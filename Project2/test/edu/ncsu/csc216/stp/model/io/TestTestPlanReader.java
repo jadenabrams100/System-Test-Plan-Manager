f4d2edac47738bc4134be83ddb798f277f8bb637
Jaden Abrams <jlabrams@ncsu.edu>
2021-10-31 17:50:46 -0400

    [Test] Wrote tests for the test plan reader

--- Project2/test/edu/ncsu/csc216/stp/model/io/TestTestPlanReader.java
+++ Project2/test/edu/ncsu/csc216/stp/model/io/TestTestPlanReader.java
@@ -5,7 +5,12 @@ package edu.ncsu.csc216.stp.model.io;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.File;
+
 import org.junit.jupiter.api.Test;
+
+import edu.ncsu.csc216.stp.model.test_plans.TestPlan;
+import edu.ncsu.csc216.stp.model.util.ISortedList;
 
 /**
  * Ensures that TestPlanReader works properly
@@ -21,6 +26,58 @@ class TestTestPlanReader {
 	@Test
 	void testReadTestPlansFile() {
 		fail("Not yet implemented");
+		
+		//test file 0
+		ISortedList<TestPlan> list1 = TestPlanReader.readTestPlansFile(new File("test-files/test-plans0.txt"));
+		assertEquals(2, list1.size());
+		assertEquals("PackScheduler", list1.get(0).getTestPlanName());
+		TestPlan t1 = list1.get(0);
+		assertEquals(2, t1.getTestCases().size());
+		assertEquals("test0", t1.getTestCase(0).getTestCaseId());
+		assertEquals("Invalid", t1.getTestCase(0).getTestType());
+		assertEquals("description", t1.getTestCase(0).getTestDescription());
+		assertEquals("expected results\nwith multiple lines", t1.getTestCase(0).getExpectedResults());
+		assertEquals(2, t1.getTestCase(0).getResults().size());
+		assertFalse(t1.getTestCase(0).isTestCasePassing());
+		assertEquals("test1", t1.getTestCase(1).getTestCaseId());
+		assertEquals("Equivalence Class", t1.getTestCase(1).getTestType());
+		assertEquals("description", t1.getTestCase(1).getTestDescription());
+		assertEquals("expected results", t1.getTestCase(1).getExpectedResults());
+		assertEquals(1, t1.getTestCase(1).getResults().size());
+		assertTrue(t1.getTestCase(1).isTestCasePassing());
+		
+		//test file 2
+		ISortedList<TestPlan> list2 = TestPlanReader.readTestPlansFile(new File("test-files/test-plans2.txt"));
+		assertEquals(1, list2.size());
+		assertEquals("WolfScheduler", list2.get(0).getTestPlanName());
+		assertEquals(0, list2.get(0).getTestCases().size());
+		// test file 3
+		Exception e1 = assertThrows(IllegalArgumentException.class, () -> TestPlanReader.readTestPlansFile(new File("test-files/test-plans3.txt")));
+		assertEquals("Unable to load file.", e1.getMessage());
+		//test file 4
+		ISortedList<TestPlan> list3 = TestPlanReader.readTestPlansFile(new File("test-files/test-plans4.txt"));
+		assertEquals("WolfScheduler", list3.get(0).getTestPlanName());
+		assertEquals(0, list3.get(0).getTestCases().size());
+		//test file 5
+		ISortedList<TestPlan> list4 = TestPlanReader.readTestPlansFile(new File("test-files/test-plans5.txt"));
+		assertEquals("WolfScheduler", list4.get(0).getTestPlanName());
+		assertEquals(0, list4.get(0).getTestCases().size());
+		//test file 6
+		ISortedList<TestPlan> list5 = TestPlanReader.readTestPlansFile(new File("test-files/test-plans6.txt"));
+		assertEquals("WolfScheduler", list5.get(0).getTestPlanName());
+		assertEquals(0, list5.get(0).getTestCases().size());
+		//test file 7
+		ISortedList<TestPlan> list6 = TestPlanReader.readTestPlansFile(new File("test-files/test-plans7.txt"));
+		assertEquals("WolfScheduler", list6.get(0).getTestPlanName());
+		assertEquals(0, list6.get(0).getTestCases().size());
+		//test file 8
+		ISortedList<TestPlan> list7 = TestPlanReader.readTestPlansFile(new File("test-files/test-plans8.txt"));
+		assertEquals("WolfScheduler", list7.get(0).getTestPlanName());
+		assertEquals(0, list7.get(0).getTestCases().size());
+		//test file 9
+		ISortedList<TestPlan> list8 = TestPlanReader.readTestPlansFile(new File("test-files/test-plans9.txt"));
+		assertEquals("WolfScheduler", list8.get(0).getTestPlanName());
+		assertEquals(0, list8.get(0).getTestCases().size());
 	}
 
 }
