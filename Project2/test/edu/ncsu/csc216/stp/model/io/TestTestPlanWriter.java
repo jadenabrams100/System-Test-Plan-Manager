977bcbd5ecc19eec5f4daf0ec6b93492ef574c9f
Jaden Abrams <jlabrams@ncsu.edu>
2021-10-31 18:26:49 -0400

    [Test] Wrote test for TestPlanWriter

--- Project2/test/edu/ncsu/csc216/stp/model/io/TestTestPlanWriter.java
+++ Project2/test/edu/ncsu/csc216/stp/model/io/TestTestPlanWriter.java
@@ -5,7 +5,14 @@ package edu.ncsu.csc216.stp.model.io;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Scanner;
+
 import org.junit.jupiter.api.Test;
+
+import edu.ncsu.csc216.stp.model.test_plans.TestPlan;
+import edu.ncsu.csc216.stp.model.util.ISortedList;
 
 /**
  * Ensures that TestPlanWriter works as expected
@@ -20,7 +27,20 @@ class TestTestPlanWriter {
 	 */
 	@Test
 	void testWriteTestPlanFile() {
-		fail("Not yet implemented");
+		ISortedList<TestPlan> list = TestPlanReader.readTestPlansFile(new File("test-files/test-plans1.txt"));
+		TestPlanWriter.writeTestPlanFile(new File("test-files/actual-test-plans.txt"), list);
+		try {
+			Scanner fileReader = new Scanner(new FileInputStream("test-files/test-plans1.txt"));
+			Scanner fileReader2 = new Scanner(new FileInputStream("test-files/actual-test-plans.txt"));
+			while(fileReader.hasNextLine() && fileReader2.hasNextLine()) {
+				assertEquals(fileReader.nextLine(), fileReader2.nextLine());
+			}
+			if(fileReader.hasNextLine() || fileReader2.hasNextLine()) {
+				fail("One of the files has too many lines");
+			}
+		} catch(Exception e){
+			fail();
+		}
 	}
 
 }
