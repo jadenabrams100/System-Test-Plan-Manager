9566fb832ed9f8465fa01ac6ff21dfbf7e955a98
Jaden Abrams <jlabrams@ncsu.edu>
2021-10-31 18:42:01 -0400

    [Implementation] partially implemented TestPlanReader

--- Project2/src/edu/ncsu/csc216/stp/model/io/TestPlanReader.java
+++ Project2/src/edu/ncsu/csc216/stp/model/io/TestPlanReader.java
@@ -4,8 +4,12 @@ import edu.ncsu.csc216.stp.model.test_plans.AbstractTestPlan;
 import edu.ncsu.csc216.stp.model.test_plans.TestPlan;
 import edu.ncsu.csc216.stp.model.tests.TestCase;
 import edu.ncsu.csc216.stp.model.util.ISortedList;
+import edu.ncsu.csc216.stp.model.util.SortedList;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 /**
  * Class that handles reading Test Plans from a file
@@ -20,6 +24,20 @@ public class TestPlanReader {
 	 * @throws IllegalArgumentException if the file cannot be loaded or does not exist.
 	 */
 	public static ISortedList<TestPlan> readTestPlansFile(File file) {
+		
+		
+		try {
+			Scanner fileReader = new Scanner(new FileInputStream(file.getPath()));
+			ISortedList<TestPlan> plans = new SortedList<TestPlan>();
+			String fileLine = "";
+			while(fileReader.hasNextLine()) {
+				
+			}
+		} catch (FileNotFoundException e) {
+			throw new IllegalArgumentException("Unable to load file.");
+		}
+		
+		
 		return null;
 	}
 
